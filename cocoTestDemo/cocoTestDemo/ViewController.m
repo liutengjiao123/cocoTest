04cef2a37a1a1e63e3fbee5ed6b199d395e4af3f
liu <1425110265@qq.com>
2021-07-09 15:26:56 +0800

    ”测试“

---
+++ cocoTestDemo/cocoTestDemo/ViewController.m
@@ -0,0 +1,22 @@
+//
+//  ViewController.m
+//  cocoTestDemo
+//
+//  Created by liutengjiao on 2021/7/9.
+//
+
+#import "ViewController.h"
+#import "PickerTool.h"
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+-(void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event{
+    
+    [PickerTool log];
+}
+
+
+@end
